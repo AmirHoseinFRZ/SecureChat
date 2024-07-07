4955287fdc2c8dc6e69303df6c84daa5eff2e73d
AmirHoseinFRZ <amirhoseinf123456@gmail.com>
2024-07-08 03:00:48 +0330

    other routes added.

--- backend/src/startup/routes.js
+++ backend/src/startup/routes.js
@@ -3,6 +3,9 @@ const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 
 const users = require('../routes/users');
+const groups = require('../routes/groups');
+const roles = require('../routes/roles');
+const permissions = require('../routes/permissions');
 const test = require('../routes/test');
 const error = require('../middleware/error');
 
@@ -12,6 +15,9 @@ module.exports = function (app) {
 	app.use(cookieParser());
 
 	app.use('/api/users', users)
+	app.use('/api/groups', groups)
+	app.use('/api/roles', roles)
+	app.use('/api/permissions', permissions)
 	app.use('/api/test', test)
 	// other routes
 	app.use(error);
